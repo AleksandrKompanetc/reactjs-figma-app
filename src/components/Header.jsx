3998110eaef659e4c74bfce0c447f65bae556c1d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-04 17:32:32 +0200

    added component Header and images folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <section className='header__main'>
+        
+      </section>
+    </div>
+  )
+}
