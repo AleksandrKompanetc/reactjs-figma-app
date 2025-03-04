178ec8d3a0232884f4030318661bdae9b3c3583e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-04 17:48:39 +0200

    added styles for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import logo from '../assets/site/images/siteImages/logo.png'
+import './Header.css'
 
 const Header = () => {
   return (
     <div className='header'>
       <section className='header__main'>
-        
+        <img src={logo} alt="" />
       </section>
     </div>
   )
 }
+
+export default Header
