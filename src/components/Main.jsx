5d27ff0069fbfa9141a313326388a6c2a7478fb1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-05 22:02:44 +0200

    changing styles for Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Main.css'
+import features from '../assets/site/images/siteImages/features.png'
 
 const Main = () => {
   return (
     <main className='main'>
-      <section className='features'>
-        <h1>Features</h1>
+      <section className='main__features'>
+        <h1 className='main__title'>Features</h1>
+        <h5 className='main__subtitle'>
+          Most calendars are designed for teams. Slate is designed for 
+        freelancers who want a simple way to plan their schedule.
+        </h5>
+        <img src={features} alt="Features" />
       </section>
     </main>
   )
