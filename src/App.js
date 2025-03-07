d970e500b211b2d6d8bc9b1f67b5744a12a9741a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-07 18:06:31 +0200

    adding component Test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Test />
     </div>
   );
 }
---
+++ src/components/Test.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='test'>
+      <div className='test1'></div>
+      <div className='test2'></div>
+      <div className='test3'></div>
+      <div className='test4'></div>
+    </div>
+  )
+}
+
+export default Test
