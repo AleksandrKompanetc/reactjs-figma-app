76ef66463cc2f771424b8b317d0b25ff961a1f4c
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-09 23:01:46 +0200

    adding changes for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,30 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='links'></div>
+      <div className='footer__links'>
+        <div className='links'>
+          <p className='link__title'>Fingertipe</p>
+          <a href="#">Home</a>
+          <a href="#">Example</a>
+          <a href="#">Pricing</a>
+          <a href="#">Updates</a>
+        </div>
+        <div className='links'>
+          <p className='link__title'>Resources</p>
+          <a href="#">Home</a>
+          <a href="#">Example</a>
+          <a href="#">Pricing</a>
+          <a href="#">Updates</a>
+        </div>
+        <div className='links'>
+          <p className='link__title'>About</p>
+          <a href="#">Home</a>
+          <a href="#">Example</a>
+          <a href="#">Pricing</a>
+          <a href="#">Updates</a>
+        </div>
+      </div>
+      <div className='footer__info'>Test</div>
     </div>
   )
 }
